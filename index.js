8584266d0c13c5f6295a6fae85dd7df3c3fa567c
Chris Pfaff <chrispfaff10@gmail.com>
2018-11-25 09:04:34 -0600

    created express server to host pdf

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static("public"));
 
 app
-  .get("/", (req, res) => {
+  .get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/index.html"));
   })
   .listen(port);
